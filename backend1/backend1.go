aefa8d38d7d0352f33d74aaf6b9a5052c23c3126
Tarik Ouabrk <tarik.ouabrk@gmail.com>
2026-01-22 21:02:24 +0100

    add first backend

---
+++ backend1/backend1.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(2 * time.Second) 
+		fmt.Fprintln(w, "Hello from backend 8082")
+	})
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte("OK"))
+})
+
+
+	http.ListenAndServe(":8082", nil)
+}
