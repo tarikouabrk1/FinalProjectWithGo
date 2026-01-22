669045dd9b20a40680ef742478788c3a5cce95c4
Tarik Ouabrk <tarik.ouabrk@gmail.com>
2026-01-22 21:02:49 +0100

    add second backend

---
+++ backend2/backend2.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(2 * time.Second) 
+		fmt.Fprintln(w, "Hello from backend 8083")
+	})
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte("OK"))
+    })
+
+	http.ListenAndServe(":8083", nil)
+}
